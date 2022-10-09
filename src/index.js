553d332c1734dd19e3621f273f95ab996e7871a9
Byron <byron.piedrahita@onthefuze.com.au>
2022-10-09 16:03:03 -0500

    Schemas

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
 const { ApolloServer } = require('apollo-server');
 
+// Data link fake
+let links = [
+    {
+        id: 'link-0',
+        url: 'www.dominofake.com',
+        description: 'Backend trainig for GraphQL'
+    },
+    {
+        id: 'link-1',
+        url: 'www.domino2fake.com',
+        description: 'Backend trainig for GraphQL ejemplo link dos'
+    }
+]
+
 // 1
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
-
 // 2
 const resolvers = {
   Query: {
-    info: () => `We are starting with the training`
+    // 1
+    info: () => `We are starting with the training`,
+    // 2
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   }
 }
 
